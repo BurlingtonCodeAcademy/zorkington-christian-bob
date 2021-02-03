4067c94cdebcfad57deb248dc3ef748163b848da
802ScubaFish <christianrdow@gmail.com>
2021-02-03 14:57:49 -0500

    new commit

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  console.log('Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with evry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!');
   process.exit();
 }
